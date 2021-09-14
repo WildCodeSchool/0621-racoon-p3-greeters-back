9ea1f688d317dd4e32964020a156751bd20295c4
Shawn <shawn17@live.fr>
2021-09-14 11:25:01 +0200

    back part config

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
