c1fb1b1ae6e69e5486d2fee67c7d6071ae70bab5
RomainM81 <dcromain112@gmail.com>
2021-09-20 16:36:30 +0100

    create post for city

--- routes/person.js
+++ routes/person.js
@@ -45,20 +45,7 @@ router.get('/:id', (req, res) => {
     }
   )
 })
-//Post d'un greeter avec toutes les données//
-=======
-  //All greeters
-  mysql.query('SELECT * FROM person', (err, result) => {
-    if (err) {
-      res.status(500).send('error from database')
-    } else {
-      res.status(200).json(result)
-    }
-  })
-  res.status(200).send('je suis dans /persons')
-})
 
->>>>>>> c2ac6827cb14b2968cd1251458e9764c64d6170d
 router.post('/', (req, res) => {
   const bodyData = [
     req.body.person_firstname,
