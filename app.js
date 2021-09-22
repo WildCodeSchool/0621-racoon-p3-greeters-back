9c6527eb946cd0547d40b13740d3581ed720857e
Shawn <shawn17@live.fr>
2021-09-22 11:12:09 +0200

    merge pull

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require('express')
+const connection = require('./db-config')
 const app = express()
+
+connection.connect(err => {
+  if (err) {
+    console.error('error connecting: ' + err.stack)
+  } else {
+    console.log('connected as id ' + connection.threadId)
+  }
+})
 
 const port = process.env.PORT || 3000
 
