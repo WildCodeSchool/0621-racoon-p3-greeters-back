1534f29944124ce5f259a4412ba562b88b559195
Dorothée Laget-Chappert <dorothee.lagetchappert@gmail.com>
2021-09-28 12:06:35 +0200

    include pictures

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connection = require('./db-config')
+const serve = require('express-static')
 const app = express()
 const cors = require('cors')
 const morgan = require('morgan')
@@ -17,6 +18,9 @@ connection.connect(err => {
     console.log('connected as id ' + connection.threadId)
   }
 })
+
+app.use(serve(__dirname + '/assets'))
+app.use(express.static('assets'))
 app.use(cors())
 app.use(morgan('tiny'))
 app.use(express.json())
@@ -26,6 +30,10 @@ app.use('/city', routes.city)
 app.use('/description', routes.description)
 app.use('/valeur', routes.valeur)
 app.use('/concept', routes.concept)
+
+const server = app.listen(3001, function () {
+  console.log('server is running at %s', server.address(3001))
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
