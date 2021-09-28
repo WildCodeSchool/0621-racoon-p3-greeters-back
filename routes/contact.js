35f9c4680665d5519e128918cb62c8eeb44c9ccd
Shawn <shawn17@live.fr>
2021-09-28 16:18:59 +0200

    creating contact route

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const mysql = require('../db-config')
 
-// const { Result } = require('express-validator')
 const Joi = require('joi')
-// const { contact } = require('.')
 
 const router = express.Router()
 
@@ -18,22 +16,23 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-  const { firstname, lastname, mail, object, message } = req.body
+  const { firstname, lastname, civility, mail, object, message } = req.body.data
 
   const { error } = Joi.object({
     mail: Joi.string().email().max(255).required(),
     firstname: Joi.string().max(255).required(),
-    lastname: Joi.string().max(255).required()
-  }).validate({ mail, firstname, lastname }, { abortEarly: false })
+    lastname: Joi.string().max(255).required(),
+    civility: Joi.string().max(10).required()
+  }).validate({ mail, firstname, lastname, civility }, { abortEarly: false })
 
   if (error) {
     res.status(422).json({ validationErrors: error.details })
   } else {
     const sql =
-      'INSERT INTO contact (contact_object, contact_message, contact_lastname, contact_firstname, contact_mail, contact_treated, contact_date) VALUES (?, ?, ?, ?, ?, false, NOW())'
+      'INSERT INTO contact (contact_object, contact_message, contact_lastname, contact_firstname, contact_civility, contact_mail, contact_treated, contact_date) VALUES (?, ?, ?, ?, ?, ?, false, NOW())'
     mysql.query(
       sql,
-      [object, message, lastname, firstname, mail],
+      [object, message, lastname, firstname, civility, mail],
       (err, result) => {
         if (err) {
           console.log(err)
