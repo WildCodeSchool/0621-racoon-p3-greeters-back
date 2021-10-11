7440bc8a96c40fc540e3fd46868da6f91b718650
RomainM81 <dcromain112@gmail.com>
2021-10-11 14:22:25 +0100

    add pictures for city and change Valeur.js into value.js

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const city = require('./city')
 const concept = require('./concept')
 const contact = require('./contact')
 const description = require('./description')
-const valeur = require('./valeur')
+const value = require('./value')
 const them = require('./thematic')
 const lang = require('./language')
 
 module.exports = {
   person,
   city,
-  valeur,
+  value,
   description,
   contact,
   concept,
