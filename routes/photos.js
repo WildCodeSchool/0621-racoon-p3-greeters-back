9518c3103d07756a31aea5989e2d7de0f02e88e4
RomainM81 <dcromain112@gmail.com>
2021-10-12 15:16:22 +0100

    add POST and DELETE routes for photos

--- routes/photos.js
+++ routes/photos.js
@@ -17,7 +17,7 @@ router.get('/', (req, res) => {
   )
 })
 
-//Get for photso by id//
+//Get for photos by id//
 router.get('/:id', (req, res) => {
   const photoId = req.params.id
   const sql = `SELECT * FROM photos JOIN city ON city.city_id=city_city_id WHERE photos.photos_id=?`
@@ -26,6 +26,37 @@ router.get('/:id', (req, res) => {
       res.status(500).send(err)
     } else {
       res.status(200).json(result)
+    }
+  })
+})
+
+//POST for photos
+router.post('/', (req, res) => {
+  const bodyData = [
+    req.body.photos_img,
+    req.body.photos_leg_fr,
+    req.body.photos_leg_en,
+    req.body.city_city_id
+  ]
+  const sql = `INSERT INTO photos (photos_img, photos_leg_fr, photos_leg_en, city_city_id) VALUES (?, ?, ?, ?)`
+  mysql.query(sql, bodyData, (err, result) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).json(result)
+    }
+  })
+})
+
+//DELETE photo by Id
+router.delete('/:id', (req, res) => {
+  const photoId = req.params.id
+  const sql = `DELETE FROM photos WHERE photos.photos_id=?`
+  mysql.query(sql, photoId, (err, result) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send('Photo deleted!')
     }
   })
 })
