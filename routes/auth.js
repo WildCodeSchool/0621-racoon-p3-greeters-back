e3ffb498a27a21c5aab62cfd53b03185438a4613
RomainM81 <dcromain112@gmail.com>
2021-10-14 17:24:15 +0100

    add jwt and link it the front

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,24 @@
 const express = require('express')
-const jwt = require('jsonwebtoken')
 const connection = require('../db-config')
+const jwt = require('jsonwebtoken')
 const db = connection.promise()
 const argon2 = require('argon2')
-const crypto = require('crypto')
 require('dotenv').config()
 const mysql = require('../db-config')
 
 const router = express.Router()
 
 const calculateToken = (userEmail = '') => {
-  return crypto
-    .createHash('md5')
-    .update(userEmail + process.env.ACCESS_TOKEN_SECRET)
-    .digest('hex')
+  return jwt.sign({ log: userEmail }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '1800s'
+  })
 }
 
 const findByLog = log => {
   return db
     .query('SELECT * FROM admin WHERE admin_log = ?', [log])
 
-    .then(
-      ([results]) => console.log('poulet ou autres', results[0]) || results[0]
-    )
+    .then(([results]) => results[0])
 }
 
 const hashingOptions = {
@@ -32,25 +28,15 @@ const hashingOptions = {
   parallelism: 1
 }
 
-const hashPassword = plainPassword => {
-  return argon2.hash(plainPassword, hashingOptions)
-}
-
 const verifyPassword = (plainPassword, hashedPassword) => {
   return argon2.verify(hashedPassword, plainPassword, hashingOptions)
 }
-
-// function generateAccesToken(adminData) {
-//   return jwt.sign(adminData, process.env.ACCESS_TOKEN_SECRET, {
-//     expiresIn: '1800s'
-//   })
-// }
 
 router.post('/', (req, res) => {
   let adminData
   mysql.query(`SELECT * FROM admin`, (err, result) => {
     if (err) {
-      status(500).send(err)
+      res.status(500).send(err)
     } else {
       adminData = result[0]
     }
@@ -59,14 +45,11 @@ router.post('/', (req, res) => {
     findByLog(log).then(u => {
       if (!u) res.status(401).send('Invalid log')
       else {
-        console.log('on est dedans!' + password)
         verifyPassword(password, adminData.admin_passw).then(passwC => {
           if (passwC) {
             const token = calculateToken(log)
-            console.log(token)
-            res.cookie('user_token', token)
-            res.send('bijou' + token)
-          } else res.status(401).send('Invalid password')
+            res.send(token)
+          } else res.send('Invalid')
         })
       }
     })
